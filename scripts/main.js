3443d6bca9c3e15ad852fba18cced3cbcb0567f5
mcoons <mcoons67@gmail.com>
2018-12-03 22:48:48 -0700

    added more CSS and started adding LEDs

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 let BOXWIDTH = 270;
-let CLOCKWIDTH = BOXWIDTH-50;
-var centerX = centerY = CLOCKWIDTH/2;
+let CLOCKWIDTH = BOXWIDTH - 50;
+var centerX = centerY = CLOCKWIDTH / 2;
 
 var overlayAlpha = .3;
-var workColor = "rgba(0, 250, 0, " + overlayAlpha + ")"; 
-var restColor = "rgba(250, 0, 0, " + overlayAlpha + ")"; 
+var workColor = "rgba( 0, 250, 0, " + overlayAlpha + " )";
+var restColor = "rgba( 250, 0, 0, " + overlayAlpha + " )";
 var workLength = 25;  // minutes
 var restLength = 5;   // minutes
 var working = resting = false;
@@ -13,166 +13,146 @@ var workStartRotation = workEndRotation = restStartRotation = restEndRotation = 
 var workStartTime = workEndTime = restStartTime = restEndTime = null;
 
 var appcontainer = document.getElementById("appcontainer");
-appcontainer.style.width = BOXWIDTH+"px";
-appcontainer.style.height = BOXWIDTH+210+"px";
+appcontainer.style.width = BOXWIDTH + "px";
+appcontainer.style.height = BOXWIDTH + 210 + "px";
 
 var container = document.getElementById("clockcontainer");
-container.style.width = container.style.height = BOXWIDTH+"px";
+container.style.width = container.style.height = BOXWIDTH + "px";
 
 var clockFace = document.getElementById("clockface");
 var faceCtx = clockFace.getContext("2d");
-clockFace.width = clockFace.height = CLOCKWIDTH; 
+clockFace.width = clockFace.height = CLOCKWIDTH;
 
 var clockShadow = document.getElementById("clockshadow");
-clockShadow.width = clockShadow.height = CLOCKWIDTH; 
+clockShadow.width = clockShadow.height = CLOCKWIDTH;
 
 var hands = document.getElementById("hands");
 var handsCtx = hands.getContext("2d");
 hands.width = hands.height = CLOCKWIDTH;
 
 var overlay = document.getElementById("overlay");
-var overlayCtx = hands.getContext("2d");
+var overlayCtx = overlay.getContext("2d");
 overlay.width = overlay.height = CLOCKWIDTH;
 
 var workLengthSlider = document.getElementById("workLengthSlider");
 var workLengthLabel = document.getElementById("workLengthLabel");
-workLengthLabel.innerHTML = "Work Length: "+workLengthSlider.value; // Display the default slider value
+workLengthLabel.innerHTML = "Work Length: " + workLengthSlider.value;
 
-// Update the current slider value (each time you drag the slider handle)
-workLengthSlider.oninput = function() {
-    workLengthLabel.innerHTML = "Work Length: "+this.value;
-}
+workLengthSlider.oninput = function () { workLengthLabel.innerHTML = "Work Length: " + this.value }
 
 var restLengthSlider = document.getElementById("restLengthSlider");
 var restLengthLabel = document.getElementById("restLengthLabel");
-restLengthLabel.innerHTML = "Break Length: "+restLengthSlider.value; // Display the default slider value
+restLengthLabel.innerHTML = "Break Length: " + restLengthSlider.value;
 
-// Update the current slider value (each time you drag the slider handle)
-restLengthSlider.oninput = function() {
-    restLengthLabel.innerHTML = "Break Length: "+this.value;
-}
+restLengthSlider.oninput = function () { restLengthLabel.innerHTML = "Break Length: " + this.value }
 
 var volumeSlider = document.getElementById("volumeSlider");
 var volumeLabel = document.getElementById("volumeLabel");
-volumeLabel.innerHTML = "Volume: "+volumeSlider.value*100; // Display the default slider value
+volumeLabel.innerHTML = "Volume: " + volumeSlider.value;
 
-// Update the current slider value (each time you drag the slider handle)
-volumeSlider.oninput = function() {
-    volumeLabel.innerHTML = "Volume: "+this.value*100;
-}
+volumeSlider.oninput = function () { volumeLabel.innerHTML = "Volume: " + this.value }
 
 var masterVolume = volumeSlider.value;
 
-// document.getElementById("workLengthInput").setAttribute("value", workLength);
-// document.getElementById("restLengthInput").setAttribute("value", restLength);
-
 drawFace();
-setInterval( refreshClock, 50);
+setInterval(refreshClock, 50);
 
-function refreshClock(){
+function refreshClock() {
     var baseTime = new Date();
-
     var hr = baseTime.getHours();
     var min = baseTime.getMinutes();
     var sec = baseTime.getSeconds();
     var milliSec = baseTime.getMilliseconds();
-    sec += milliSec/1000;
+    sec += milliSec / 1000;
 
-    var hrRotation = (hr*360/12+(min*(360/60)/12))*Math.PI/180;
-    var minRotation = ((min*360/60)+(sec*(360/60)/60))*Math.PI/180;
-    var secRotation = (sec*360/60)*Math.PI/180;
+    var hrRotation = (hr * 360 / 12 + (min * (360 / 60) / 12)) * Math.PI / 180;
+    var minRotation = ((min * 360 / 60) + (sec * (360 / 60) / 60)) * Math.PI / 180;
+    var secRotation = (sec * 360 / 60) * Math.PI / 180;
 
     drawHands(hrRotation, minRotation, secRotation);
 
-    if (working && workEndTime < baseTime && !alarm1) {
-        alarm1 = soundAlarm1();
-        document.getElementById("lcd").innerHTML = "BREAK TIME&#10;PRESS 'START BREAKING'";
+    if (working && workEndTime < baseTime) {
+        if (!alarm1) alarm1 = soundAlarm1();
+        document.getElementById("lcd").innerHTML = "BREAK TIME" + (masterVolume === 0 ? " (MUTED)" : "") + "&#10;" + (baseTime.getSeconds() % 2 === 0 ? "PRESS 'START BREAKING'" : "");
     } else
-    if (working && workEndTime > baseTime){
-        let timeDiff = (workEndTime-baseTime)/ 60 / 1000;
-        let minutesLeft = Math.trunc(timeDiff);
-        let secondsLeft = ("0"+(Math.trunc(timeDiff%1*60).toString())).slice(-2);
-        document.getElementById("lcd").innerHTML = "WORKING&#10;"+minutesLeft+":"+ secondsLeft;
-    }
+        if (working && workEndTime > baseTime) {
+            let timeDiff = (workEndTime - baseTime) / 60 / 1000;
+            let minutesLeft = Math.trunc(timeDiff);
+            let secondsLeft = ("0" + (Math.trunc(timeDiff % 1 * 60).toString())).slice(-2);
+            document.getElementById("lcd").innerHTML = "WORKING" + (masterVolume === 0 ? " (MUTED)" : "") + "&#10;" + minutesLeft + ":" + secondsLeft;
+        }
 
-    if (resting && restEndTime < baseTime && !alarm2) {
-        alarm2 = soundAlarm2();
-        document.getElementById("lcd").innerHTML = "TIME TO WORK&#10;PRESS 'START WORKING'";
+    if (resting && restEndTime < baseTime) {
+        if (!alarm2) alarm2 = soundAlarm2();
+        document.getElementById("lcd").innerHTML = "TIME TO WORK" + (masterVolume === 0 ? " (MUTED)" : "") + "&#10;" + (baseTime.getSeconds() % 2 === 0 ? "PRESS 'START WORKING'" : "");
     } else
-    if (resting && restEndTime > baseTime){
-        let timeDiff = (restEndTime-baseTime)/ 60 / 1000;
-        let minutesLeft = Math.trunc(timeDiff);
-        let secondsLeft = ("0"+(Math.trunc(timeDiff%1*60).toString())).slice(-2);
-        document.getElementById("lcd").innerHTML = "TAKING A BREAK&#10;"+minutesLeft+":"+ secondsLeft;
-    }
+        if (resting && restEndTime > baseTime) {
+            let timeDiff = (restEndTime - baseTime) / 60 / 1000;
+            let minutesLeft = Math.trunc(timeDiff);
+            let secondsLeft = ("0" + (Math.trunc(timeDiff % 1 * 60).toString())).slice(-2);
+            document.getElementById("lcd").innerHTML = "TAKING A BREAK" + (masterVolume === 0 ? " (MUTED)" : "") + "&#10;" + minutesLeft + ":" + secondsLeft;
+        }
 
-    if (working || resting){
-        drawOverlays(workStartRotation, workEndRotation, restStartRotation, restEndRotation);
-    }
-
-    if (!working && !resting){
-        document.getElementById("lcd").innerHTML = "CLOCK MODE&#10;"+baseTime.toLocaleTimeString();
-    }
+    if (working || resting) drawOverlays(workStartRotation, workEndRotation, restStartRotation, restEndRotation);
+    if (!working && !resting) document.getElementById("lcd").innerHTML = "CLOCK MODE&#10;" + baseTime.toLocaleTimeString();
 }
 
-function drawFace(){
-   faceCtx.font = CLOCKWIDTH/10 + "px Serif";
-    
+function drawFace() {
+    faceCtx.font = CLOCKWIDTH / 10 + "px Serif";
+
     // draw circle
     faceCtx.beginPath();
-    faceCtx.arc(centerX, centerY, CLOCKWIDTH/2, 0, 2 * Math.PI, false);
+    faceCtx.arc(centerX, centerY, CLOCKWIDTH / 2, 0, 2 * Math.PI, false);
     faceCtx.fillStyle = 'white';
     faceCtx.fill();
     faceCtx.lineWidth = 2;
     faceCtx.strokeStyle = '#000000';
     faceCtx.stroke();
-    
-    for (let degrees = 0; degrees < 360; degrees+=6){
-        let rad = degrees*Math.PI/180;  
-        let tickLength = CLOCKWIDTH/72;
-        faceCtx.lineWidth=1;
- 
-        if (degrees%90===0){ 
-            tickLength=CLOCKWIDTH/24;
-            faceCtx.lineWidth=CLOCKWIDTH/60; 
-        }  
-        else 
-        if (degrees%30===0){ 
-            tickLength=CLOCKWIDTH/48;
-            faceCtx.lineWidth=CLOCKWIDTH/120; 
-        }  
-        
-        let x1 = Math.cos(rad)*CLOCKWIDTH/2+centerX;
-        let y1 = Math.sin(rad)*CLOCKWIDTH/2+centerY;
-        let x2 = Math.cos(rad)*(CLOCKWIDTH/2-tickLength)+centerX;
-        let y2 = Math.sin(rad)*(CLOCKWIDTH/2-tickLength)+centerY;
-        
+
+    for (let degrees = 0; degrees < 360; degrees += 6) {
+        let rad = degrees * Math.PI / 180;
+        let tickLength = CLOCKWIDTH / 70;
+        faceCtx.lineWidth = 1;
+
+        if (degrees % 90 === 0) {
+            tickLength = CLOCKWIDTH / 20;
+            faceCtx.lineWidth = CLOCKWIDTH / 60;
+        }
+        else
+            if (degrees % 30 === 0) {
+                tickLength = CLOCKWIDTH / 40;
+                faceCtx.lineWidth = CLOCKWIDTH / 120;
+            }
+
+        let x1 = Math.cos(rad) * CLOCKWIDTH / 2 + centerX;
+        let y1 = Math.sin(rad) * CLOCKWIDTH / 2 + centerY;
+        let x2 = Math.cos(rad) * (CLOCKWIDTH / 2 - tickLength) + centerX;
+        let y2 = Math.sin(rad) * (CLOCKWIDTH / 2 - tickLength) + centerY;
+
         faceCtx.beginPath();
-        faceCtx.moveTo(x1,y1);
-        faceCtx.lineTo(x2,y2);
+        faceCtx.moveTo(x1, y1);
+        faceCtx.lineTo(x2, y2);
         faceCtx.stroke();
     }
 
     // Draw face text
     faceCtx.fillStyle = '#967A45';
-    faceCtx.fillText("XII",CLOCKWIDTH/2-16, 30);
-    faceCtx.fillText("III",CLOCKWIDTH-35, CLOCKWIDTH/2+8);
-    faceCtx.fillText("VI",CLOCKWIDTH/2-14, CLOCKWIDTH - 13);
-    faceCtx.fillText("IX",13, CLOCKWIDTH/2+8);
+    faceCtx.fillText("XII", CLOCKWIDTH / 2 - 16, 30);
+    faceCtx.fillText("III", CLOCKWIDTH - 35, CLOCKWIDTH / 2 + 8);
+    faceCtx.fillText("VI", CLOCKWIDTH / 2 - 14, CLOCKWIDTH - 13);
+    faceCtx.fillText("IX", 13, CLOCKWIDTH / 2 + 8);
 
     faceCtx.font = "12px Serif";
-    faceCtx.fillText("Pomodoro",CLOCKWIDTH/2-26, CLOCKWIDTH/2+20);
-    faceCtx.fillText("by",CLOCKWIDTH/2-5, CLOCKWIDTH/2+30);
-    faceCtx.fillText("Coons",CLOCKWIDTH/2-14, CLOCKWIDTH/2+40);
+    faceCtx.fillText("Pomodoro", CLOCKWIDTH / 2 - 26, CLOCKWIDTH / 2 + 20);
+    faceCtx.fillText("by", CLOCKWIDTH / 2 - 5, CLOCKWIDTH / 2 + 30);
+    faceCtx.fillText("Coons", CLOCKWIDTH / 2 - 14, CLOCKWIDTH / 2 + 40);
 }
 
-function drawHands(hrRotation, minRotation, secRotation){
+function drawHands(hrRotation, minRotation, secRotation) {
     handsCtx.clearRect(0, 0, CLOCKWIDTH, CLOCKWIDTH);
 
     handsCtx.fillStyle = 'black';
-    drawHand(hrRotation, 5, -CLOCKWIDTH/2+CLOCKWIDTH/6);
-    drawHand(minRotation, 3, -CLOCKWIDTH/2+CLOCKWIDTH/15);
- 
+    
     // MINUTE HAND AXIS CIRCLE
     handsCtx.resetTransform();
     handsCtx.beginPath();
@@ -181,10 +161,12 @@ function drawHands(hrRotation, minRotation, secRotation){
     handsCtx.fill();
     handsCtx.strokeStyle = '#000000';
     handsCtx.stroke();
+    
+    drawHand(hrRotation, 6, -CLOCKWIDTH / 2 + CLOCKWIDTH / 6);
+    drawHand(minRotation, 4, -CLOCKWIDTH / 2 + CLOCKWIDTH / 15);
 
     handsCtx.fillStyle = 'red';
-    drawHand(secRotation, 1, -CLOCKWIDTH/2+CLOCKWIDTH/30);
- 
+    
     // SECOND HAND AXIS CIRCLE
     handsCtx.resetTransform();
     handsCtx.beginPath();
@@ -193,31 +175,39 @@ function drawHands(hrRotation, minRotation, secRotation){
     handsCtx.fill();
     handsCtx.strokeStyle = '#ff0000';
     handsCtx.stroke();
+    
+    drawHand(secRotation, 2, -CLOCKWIDTH / 2 + CLOCKWIDTH / 30);
+
 }
 
-function drawHand(rotation, handWidth, handLength){
+function drawHand(rotation, handWidth, handLength) {
     handsCtx.resetTransform();
     handsCtx.translate(centerX, centerY);
     handsCtx.rotate(rotation);
     handsCtx.translate(-centerX, -centerY);
-    handsCtx.fillRect(centerX-handWidth/2, centerY, handWidth, handLength);
+    handsCtx.shadowBlur = 4;
+    handsCtx.shadowColor="gray";
+    handsCtx.shadowOffsetX = 3;
+    handsCtx.shadowOffsetY = 3;
+    handsCtx.fillRect(centerX - handWidth / 2, centerY, handWidth, handLength);
 }
 
-function drawOverlays(workStartRotation, workEndRotation, restStartRotation, restEndRotation){
+function drawOverlays(workStartRotation, workEndRotation, restStartRotation, restEndRotation) {
+    overlayCtx.clearRect(0, 0, CLOCKWIDTH, CLOCKWIDTH);
     drawOverlay(overlayCtx, workStartRotation, workEndRotation, workColor);
     drawOverlay(overlayCtx, restStartRotation, restEndRotation, restColor);
 }
 
-function drawOverlay(ctx, start, end, color){
+function drawOverlay(ctx, start, end, color) {
     ctx.beginPath();
-    ctx.moveTo(CLOCKWIDTH/2, CLOCKWIDTH/2);
-    ctx.arc(CLOCKWIDTH/2, CLOCKWIDTH/2, CLOCKWIDTH/2, start, end, false);
+    ctx.moveTo(CLOCKWIDTH / 2, CLOCKWIDTH / 2);
+    ctx.arc(CLOCKWIDTH / 2, CLOCKWIDTH / 2, CLOCKWIDTH / 2, start, end, false);
     ctx.closePath();
-    ctx.fillStyle = color; 
+    ctx.fillStyle = color;
     ctx.fill();
 }
 
-function calculateWorkRestRotations(starting){
+function calculateWorkRestRotations(starting) {
     let baseTime = new Date(starting.getTime());
     let sec = baseTime.getSeconds();
 
@@ -226,135 +216,90 @@ function calculateWorkRestRotations(starting){
     let newWorkMinutes = workEndTime.getMinutes() + Number(workLength);
     workEndTime.setMinutes(newWorkMinutes);
 
-    workStartRotation = ((workStartTime.getMinutes()*360/60)+(sec*(360/60)/60))*Math.PI/180 - Math.PI/2;
-    workEndRotation = ((workEndTime.getMinutes()*360/60)+(sec*(360/60)/60))*Math.PI/180 - Math.PI/2;
+    workStartRotation = ((workStartTime.getMinutes() * 360 / 60) + (sec * (360 / 60) / 60)) * Math.PI / 180 - Math.PI / 2;
+    workEndRotation = ((workEndTime.getMinutes() * 360 / 60) + (sec * (360 / 60) / 60)) * Math.PI / 180 - Math.PI / 2;
 
     restStartTime = new Date(workEndTime.getTime());
     restEndTime = new Date(restStartTime.getTime());
     let newRestMinutes = restEndTime.getMinutes() + Number(restLength);
     restEndTime.setMinutes(newRestMinutes);
 
-    restStartRotation = ((restStartTime.getMinutes()*360/60)+(sec*(360/60)/60))*Math.PI/180 - Math.PI/2;
-    restEndRotation = ((restEndTime.getMinutes()*360/60)+(sec*(360/60)/60))*Math.PI/180 - Math.PI/2;
-
+    restStartRotation = ((restStartTime.getMinutes() * 360 / 60) + (sec * (360 / 60) / 60)) * Math.PI / 180 - Math.PI / 2;
+    restEndRotation = ((restEndTime.getMinutes() * 360 / 60) + (sec * (360 / 60) / 60)) * Math.PI / 180 - Math.PI / 2;
 }
 
-function calculateRestWorkRotations(starting){
+function calculateRestWorkRotations(starting) {
     let baseTime = new Date(starting);
     let sec = baseTime.getSeconds();
 
-    // console.log("baseTime: ", baseTime);
-
     restStartTime = new Date(baseTime.getTime());
     restEndTime = new Date(restStartTime.getTime());
-    // console.log("first restEndTime: ",restEndTime);
-
-    // console.log("restEndTime minutes: ",restEndTime.getMinutes())
     let newRestMinutes = restEndTime.getMinutes() + Number(restLength);
-    // console.log("newRestMinutes: ", newRestMinutes);
-
     restEndTime.setMinutes(newRestMinutes);
 
-    // console.log("restStartTime: ",restStartTime);
-    // console.log("restEndTime: ",restEndTime);
-
-    restStartRotation = ((restStartTime.getMinutes()*360/60)+(sec*(360/60)/60))*Math.PI/180 - Math.PI/2;
-    restEndRotation = ((restEndTime.getMinutes()*360/60)+(sec*(360/60)/60))*Math.PI/180 - Math.PI/2;
+    restStartRotation = ((restStartTime.getMinutes() * 360 / 60) + (sec * (360 / 60) / 60)) * Math.PI / 180 - Math.PI / 2;
+    restEndRotation = ((restEndTime.getMinutes() * 360 / 60) + (sec * (360 / 60) / 60)) * Math.PI / 180 - Math.PI / 2;
 
     workStartTime = new Date(restEndTime.getTime());
     workEndTime = new Date(workStartTime.getTime());
     let newWorkMinutes = workEndTime.getMinutes() + Number(workLength);
     workEndTime.setMinutes(newWorkMinutes);
 
-    workStartRotation = ((workStartTime.getMinutes()*360/60)+(sec*(360/60)/60))*Math.PI/180 - Math.PI/2;
-    workEndRotation = ((workEndTime.getMinutes()*360/60)+(sec*(360/60)/60))*Math.PI/180 - Math.PI/2;
+    workStartRotation = ((workStartTime.getMinutes() * 360 / 60) + (sec * (360 / 60) / 60)) * Math.PI / 180 - Math.PI / 2;
+    workEndRotation = ((workEndTime.getMinutes() * 360 / 60) + (sec * (360 / 60) / 60)) * Math.PI / 180 - Math.PI / 2;
 }
 
-function optionsButtonClick(){
+function optionsButtonClick() {
     soundClick();
-    document.getElementById('optionsModal').style.marginBottom='0'; 
+    document.getElementById('optionsModal').style.marginBottom = '0';
 }
 
-function instructionsButtonClick(){
+function instructionsButtonClick() {
     soundClick();
-    document.getElementById('instructionsModal').style.marginBottom='0'; 
+    document.getElementById('instructionsModal').style.marginBottom = '0';
 }
 
-
-
-function workButtonClick(){
-
-    // console.log("-------------------------");
-    // console.log("workLength: ", workLength);
-    // console.log("restLength: ", restLength);
-
+function workButtonClick() {
     soundClick();
     stopSounds();
-
-    working=true; 
-    resting=false; 
-
+    working = true;
+    resting = false;
     calculateWorkRestRotations(new Date());
-
-    document.getElementById("lcd").innerHTML = "WORKING";
-
+    document.getElementById("lcd").innerHTML = "WORKING" + (masterVolume === 0 ? " (MUTED)" : "");
 }
 
-function restButtonClick(){
-
-    // console.log("-------------------------");
-    // console.log("workLength: ", workLength);
-    // console.log("restLength: ", restLength);
-
+function restButtonClick() {
     soundClick();
     stopSounds();
-
-    resting=true; 
-    working=false; 
-
+    resting = true;
+    working = false;
     calculateRestWorkRotations(new Date());
-
-    document.getElementById("lcd").innerHTML = "TAKING A BREAK";
-
+    document.getElementById("lcd").innerHTML = "TAKING A BREAK" + (masterVolume === 0 ? " (MUTED)" : "");
 }
 
-function clearButtonClick(){
+function clearButtonClick() {
     soundClick();
     stopSounds();
-
-    resting=false; 
-    working=false;
-
-    document.getElementById("lcd").innerHTML = "CLOCK&#10;MODE";
-
+    resting = false;
+    working = false;
+    document.getElementById("lcd").innerHTML = "CLOCK MODE";
+    overlayCtx.clearRect(0, 0, CLOCKWIDTH, CLOCKWIDTH);
 }
 
-function saveOptionsButtonClick(){
+function saveOptionsButtonClick() {
     soundClick();
-    document.getElementById('optionsModal').style.marginBottom='-215px';
+    document.getElementById('optionsModal').style.marginBottom = '-215px';
 
-    let newWorkLength = workLengthSlider.value;;
-    if (newWorkLength != workLength ) {
-        workLength = newWorkLength;
-    }
+    workLength = workLengthSlider.value;
+    restLength = restLengthSlider.value;
+    masterVolume = volumeSlider.value / 100;
 
-    let newRestLength = restLengthSlider.value;;
-    if (newRestLength != restLength ) {
-        restLength = newRestLength;
-    }
-
-    masterVolume = volumeSlider.value;
-
-    if (working){
-        calculateWorkRestRotations(new Date(workStartTime));
-    }
-
-    if (resting){
-        calculateRestWorkRotations(new Date(restStartTime));
-    }    
+    if (working) calculateWorkRestRotations(new Date(workStartTime));
+    else
+    if (resting) calculateRestWorkRotations(new Date(restStartTime));
 }
 
-function muteButtonClick(){
+function muteButtonClick() {
     stopSounds();
     masterVolume = 0;
     volumeSlider.value = 0;
