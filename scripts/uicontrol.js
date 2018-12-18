e4c3db321007924d70c0cac1fb917f52d31bc2db
mcoons <mcoons67@gmail.com>
2018-12-18 00:18:19 -0700

    added optios tabs

--- scripts/uicontrol.js
+++ scripts/uicontrol.js
@@ -18,6 +18,9 @@ volumeSlider.oninput = function () { volumeLabel.innerHTML = "Volume: " + this.v
 
 var masterVolume = volumeSlider.value / 100;
 
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 function optionsButtonClick() {
     soundClick();
     document.getElementById('optionsModal').style.marginBottom = '0';
@@ -74,3 +77,17 @@ function muteButtonClick() {
     volumeSlider.value = 0;
     volumeLabel.innerHTML = "Volume: " + volumeSlider.value;
 }
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
